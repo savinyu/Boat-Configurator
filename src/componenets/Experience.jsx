f79cd101b02ad403e75eeb98f6435f2ece6e75ab
Savinyu <savinyupant@gmail.com>
2024-06-15 23:53:36 +0530

    vercel update

--- src/componenets/Experience.jsx
+++ src/componenets/Experience.jsx
@@ -17,8 +17,8 @@ export default function Experience(){
         <Suspense fallback={null}>
             <Float 
                 floatIntensity={2}
-                rotationIntensity={1}
-                speed={2}
+                rotationIntensity={0.8}
+                speed={1.5}
             >
                 <mesh scale={0.01} >
                     <Headphone />
